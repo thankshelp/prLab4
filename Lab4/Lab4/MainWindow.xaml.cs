8e3d829ae54fc7a7879240dde0452922be7212fc
thankshelp <taraninav.44@gmail.com>
2019-04-23 13:21:30 +0700

    pochti gotovo

--- Lab4/Lab4/MainWindow.xaml.cs
+++ Lab4/Lab4/MainWindow.xaml.cs
@@ -26,7 +26,7 @@ namespace Lab4
         public string FIO { get; set; }
         public int phys { get; set; }
         public int math { get; set; }
-        public int schet { get; set; }
+        
     }
 
     public partial class MainWindow : Window
@@ -40,9 +40,15 @@ namespace Lab4
             
             OpenFileDialog dlg = new OpenFileDialog();
              dlg.ShowDialog();
-
-            m_dbConnection = new SQLiteConnection("Data Source=" + dlg.FileName + ";Version=3;");
-            m_dbConnection.Open();
+            try
+            {
+                m_dbConnection = new SQLiteConnection("Data Source=" + dlg.FileName + ";Version=3;");
+                m_dbConnection.Open();
+            }
+            catch(ArgumentException)
+            {
+                MessageBox.Show("Вы не выбрали файл!");
+            }
             
         }
         
@@ -59,30 +65,55 @@ namespace Lab4
                 //string str = adt.fio.Text;
                 //int f = int.Parse(adt.fiz.Text);
                 //int m = int.Parse(adt.mat.Text);
+                try
+                {
+                    if ((int.Parse(adt.fiz.Text) <= 5) && (int.Parse(adt.mat.Text) <= 5))
+                    {
+                        string sql = "INSERT INTO students (uid, FIO) VALUES (" + adt.num.Text + ",'" + adt.fio.Text + "')";
+                        string gr = "INSERT INTO grades (uid, phys, math) VALUES (" + adt.num.Text + "," + adt.fiz.Text + "," + adt.mat.Text + ")";
 
-                string sql = "INSERT INTO students (uid, FIO) VALUES (" + adt.num.Text + ",'" + adt.fio.Text + "')";
-                string gr = "INSERT INTO grades (uid, phys, math) VALUES (" + adt.num.Text + "," + adt.fiz.Text + "," + adt.mat.Text + ")";
+                        SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+                        SQLiteCommand grad = new SQLiteCommand(gr, m_dbConnection);
+                        command.ExecuteNonQuery();
+                        grad.ExecuteNonQuery();
 
-                SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
-                SQLiteCommand grad = new SQLiteCommand(gr, m_dbConnection);
-                command.ExecuteNonQuery();
-                grad.ExecuteNonQuery();
+                        var data = new CTest { uid = int.Parse(adt.num.Text), math = int.Parse(adt.mat.Text), phys = int.Parse(adt.fiz.Text), FIO = adt.fio.Text };
+                        datag.Items.Add(data);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Введены не верные оценки!");
+                    }
+                }
+                catch (SQLiteException)
+                {
+                    MessageBox.Show("Неверный формат записи!");
+                }
             }
         }
 
         private void open_Click(object sender, RoutedEventArgs e)
         {
-            datag. = "";
-            string sql = "SELECT * FROM students, grades WHERE students.uid = grades.uid ORDER BY uid";
+            datag.Items.Clear();
+            
+                string sql = "SELECT * FROM students, grades WHERE students.uid = grades.uid ORDER BY uid";
             SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
             SQLiteDataReader reader = command.ExecuteReader();
 
             while (reader.Read())
             {
-                var data = new CTest { uid = int.Parse(reader["uid"].ToString()), math = int.Parse(reader["math"].ToString()), phys = int.Parse(reader["phys"].ToString()), FIO = reader["FIO"].ToString(), schet = int.Parse(reader["schet"].ToString()) };
-                datag.Items.Add(data);
-                //lb.Items.Add(reader["math"].ToString());
-                //lb.Items.Add(reader["FIO"].ToString());
+                if ((int.Parse(reader["math"].ToString()) <= 5) && (int.Parse(reader["phys"].ToString()) <= 5))
+                {
+                    var data = new CTest { uid = int.Parse(reader["uid"].ToString()), math = int.Parse(reader["math"].ToString()), phys = int.Parse(reader["phys"].ToString()), FIO = reader["FIO"].ToString() };
+                    datag.Items.Add(data);
+                    //lb.Items.Add(reader["math"].ToString());
+                    //lb.Items.Add(reader["FIO"].ToString());
+                }
+                else
+                {
+                    MessageBox.Show("Введены не верные оценки!");
+                    break;
+                }
             }
 
 
@@ -90,57 +121,84 @@ namespace Lab4
 
         private void Red_Click(object sender, RoutedEventArgs e)
         {
-            CTest test = (CTest)datag.SelectedItem;
-            
-            adt = new Add(test.uid.ToString(), test.FIO.ToString(), test.phys.ToString(), test.math.ToString());
-
-            if (adt.ShowDialog() == true)
+            if (datag.SelectedIndex > -1)
             {
-                //string sql = "INSERT INTO students (uid, FIO) VALUES (" + adt.num.Text + ",'" + adt.fio.Text + "')";
-                //string gr = "INSERT INTO grades (uid, phys, math) VALUES (" + adt.num.Text + "," + adt.fiz.Text + "," + adt.mat.Text + ")";
-                test.uid = int.Parse(adt.num.Text);
-                test.FIO = adt.fio.Text;
-                test.phys = int.Parse(adt.fiz.Text);
-                test.math = int.Parse(adt.mat.Text);
-                
+                CTest test = (CTest)datag.SelectedItem;
+
+                adt = new Add(test.uid.ToString(), test.FIO.ToString(), test.phys.ToString(), test.math.ToString());
+
+                if (adt.ShowDialog() == true)
+                {
+                    try
+                    {
+                        //string sql = "INSERT INTO students (uid, FIO) VALUES (" + adt.num.Text + ",'" + adt.fio.Text + "')";
+                        //string gr = "INSERT INTO grades (uid, phys, math) VALUES (" + adt.num.Text + "," + adt.fiz.Text + "," + adt.mat.Text + ")";
+                        test.uid = int.Parse(adt.num.Text);
+                        test.FIO = adt.fio.Text;
+                        test.phys = int.Parse(adt.fiz.Text);
+                        test.math = int.Parse(adt.mat.Text);
 
 
-                int si = datag.SelectedIndex;
 
-                datag.Items.RemoveAt(si);
+                        int si = datag.SelectedIndex;
 
-                var data = new CTest { uid = test.uid, FIO = test.FIO, phys = test.phys, math = test.math, schet = si + 1 };
-                datag.Items.Insert(si, data);
-                datag.Items.Refresh();
-                
+                        datag.Items.RemoveAt(si);
 
-                string sql = "UPDATE students SET uid = " + adt.num.Text + ", FIO ='"+ adt.fio.Text+"' WHERE schet = "+ test.schet.ToString();
-                string gr = "UPDATE grades SET uid = " + adt.num.Text + ", phys =" + adt.fiz.Text + ", math =" + adt.mat.Text + " WHERE schet = " + test.schet.ToString(); 
-                SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
-                SQLiteCommand grad = new SQLiteCommand(gr, m_dbConnection);
+                        var data = new CTest { uid = test.uid, FIO = test.FIO, phys = test.phys, math = test.math };
+                        datag.Items.Insert(si, data);
+                        datag.Items.Refresh();
 
-                command.ExecuteNonQuery();
-                grad.ExecuteNonQuery();
 
-                //datag.Items.RemoveAt
-                //datag.Items.Insert
+                        string sql = "UPDATE students SET uid = " + adt.num.Text + ", FIO ='" + adt.fio.Text + "' WHERE uid = " + test.uid.ToString();
+                        string gr = "UPDATE grades SET uid = " + adt.num.Text + ", phys =" + adt.fiz.Text + ", math =" + adt.mat.Text + " WHERE uid = " + test.uid.ToString();
+                        SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+                        SQLiteCommand grad = new SQLiteCommand(gr, m_dbConnection);
+
+                        command.ExecuteNonQuery();
+                        grad.ExecuteNonQuery();
+                    }
+                    catch (FormatException)
+                    {
+                    MessageBox.Show("Неверный формат записи!");
+                    }
+                    catch (SQLiteException)
+                    {
+                        MessageBox.Show("Неверный формат записи!");
+                    }
+                    catch (ArgumentOutOfRangeException)
+                    {
+                        MessageBox.Show("База данных пуста или вы не вбрали элемент!");
+                    }
+                }
             }
-
+            else
+            {
+                MessageBox.Show("База данных пуста!");
+            }
         }
 
         private void Del_Click(object sender, RoutedEventArgs e)
         {
-            CTest test = (CTest)datag.SelectedItem;
+            if (datag.SelectedIndex > -1)
+            {
+                
+                    CTest test = (CTest)datag.SelectedItem;
 
-            datag.Items.RemoveAt(datag.SelectedIndex);
+                    datag.Items.RemoveAt(datag.SelectedIndex);
 
-            string sql = "DELETE FROM students WHERE schet =" + test.schet;
-            string gr = "DELETE FROM grades WHERE schet =" + test.schet;
+                    string sql = "DELETE FROM students WHERE uid =" + test.uid;
+                    string gr = "DELETE FROM grades WHERE uid =" + test.uid;
 
-            SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
-            SQLiteCommand grad = new SQLiteCommand(gr, m_dbConnection);
-            command.ExecuteNonQuery();
-            grad.ExecuteNonQuery();
+                    SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+                    SQLiteCommand grad = new SQLiteCommand(gr, m_dbConnection);
+                    command.ExecuteNonQuery();
+                    grad.ExecuteNonQuery();
+                
+            }
+            else
+            {
+                MessageBox.Show("База данных пуста или вы не вбрали элемент!");
+            }
         }
     }
 }
